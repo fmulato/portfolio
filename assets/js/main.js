3668e69a7c3e5cb538c377c2e6aed6f815f8e42a
fmulato <fmulato@yahoo.com>
2026-03-03 09:07:50 +1300

    subtitle and speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,9 +170,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 80,
+      backSpeed: 60,
+      backDelay: 3000
     });
   }
 
